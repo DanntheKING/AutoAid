9d98b48cf2903dc5c64bc465a2d58e4e6ee561b9
DanntheKING <Danielwinston669485@gmail.com>
2023-01-06 16:14:26 -0600

    Template to AutoAid

--- app/src/main/java/com/example/autoaid/MenuActivity.kt
+++ app/src/main/java/com/example/autoaid/MenuActivity.kt
@@ -1,4 +1,13 @@
 package com.example.autoaid
 
-class MenuActivity {
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MenuActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu)
+    }
+
 }
